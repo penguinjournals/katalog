6b2cec744f2e9b2c39e12a11191520534b8c3870
David González <dgonzalezdiez@gmail.com>
2016-10-07 08:18:19 +0200

    [Switch to english] Added the persist to file feat.

--- src/main/java/net/programania/Log.java
+++ src/main/java/net/programania/Log.java
@@ -1,18 +1,30 @@
 package net.programania;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Log 
 {   
 	List<String> logBuffer = new ArrayList<String>();
+	private static final String FILENAME = "output.log";
+	
     public void add( String line )
     {
     	logBuffer.add(line);
     }
 	public void persist() {
-		for (int line = 0; line < logBuffer.size(); line++) {
-			System.out.println(logBuffer.get(line));
+		try {
+			FileWriter file = new FileWriter(FILENAME, true);
+			for (int line = 0; line < logBuffer.size(); line++) {
+				file.write(logBuffer.get(line) + "\n");
+			}
+			file.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Couldn't write to file");
+			e.printStackTrace();
 		}
 	}
 }
