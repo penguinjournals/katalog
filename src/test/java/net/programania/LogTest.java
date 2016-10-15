cef9a4696152ce123b78666f1ab274691c7f5ad1
David González <dgonzalezdiez@gmail.com>
2016-10-15 07:57:23 +0200

    File persist test. Bad approach, I'm testing Java.File instead of my code

--- src/test/java/net/programania/LogTest.java
+++ src/test/java/net/programania/LogTest.java
@@ -1,23 +1,45 @@
 package net.programania;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import junit.framework.TestCase;
 
 public class LogTest 
     extends TestCase
 {
-	public void testLog()
+	public void testLog() throws IOException
     {
     	Log log = new Log();
     	log.addLine("boniclo");
-    	log.addLine("piticli");
     	log.persist();
+    	try {
+    		File logfile = new File("output.log");
+			FileReader logfileReader = new FileReader(logfile);
+			BufferedReader reader = new BufferedReader(logfileReader);
+			String line = null;
+			String current;
+			while ((current = reader.readLine()) != null){
+				line = current;
+			}
+			reader.close();
+			assertEquals("boniclo", line);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
-	
+	/*
 	public void testMailLog()
     {
     	MailLog log = new MailLog();
     	log.addLine("boniclo");
     	log.addLine("piticli");
     	log.persist();
-    }	
+    }
+    */	
 }
