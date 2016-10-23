e8f27c54bf9fc6c4a91ae3790df7500f96f6c5ff
David González <dgonzalezdiez@gmail.com>
2016-10-23 21:24:33 +0200

    First approach to mocking

--- src/test/java/net/programania/LogTest.java
+++ src/test/java/net/programania/LogTest.java
@@ -3,18 +3,20 @@ package net.programania;
 import junit.framework.TestCase;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.mockito.Mockito;
 
 public class LogTest extends TestCase {
   public void testLog() throws IOException {
-    InMemoryDriver logDriver = new InMemoryDriver();
+	LogDriver logDriver = Mockito.mock(LogDriver.class);
     Log log = new Log(logDriver);
     String expectedLog = "boniclo";
     log.addLine(expectedLog);
     log.persist();
-    List<String> retrievedLog = logDriver.retrieveLog();
-
-    assertEquals(retrievedLog.size(), 1);
-    assertEquals(expectedLog, retrievedLog.get(0));
+    List<String> logBuffer = new ArrayList<>();
+    logBuffer.add(expectedLog);
+    Mockito.verify(logDriver,Mockito.times(1)).persist(logBuffer);
   }
 }
